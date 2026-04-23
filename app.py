65596de285f602f85f63376b4dba341afba59979
Ana Conrado <anacost@users.noreply.github.com>
2026-04-23 11:59:31 -0300

    images

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
+import os
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
+IMG_folder = os.path.join("static", "IMG")
+app.config["UPLOAD_FOLDER"] = IMG_FOLDER
 
 @app.route('/')
 def index():
-
-        return render_template('index.html')
+    IMG_LIST = os.listdir("static/IMG")
+    IMG_LIST = ["IMG/" + i for i in IMG_LIST]
+    return render_template('index.html', imagelist=IMG_LIST)
     
 @app.route('/cakes')
 def cakes():
     return render_template('cakes.html')
 
 if __name__ == '__main__':
-    app.run(debug = True, host='0.0.0.0')
+    app.run(debug = True)
